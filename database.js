42e3dbcec384c5de1870d94c176daa7443aa7268
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-06-14 20:26:01 +0200

    week-2

--- database.js
+++ database.js
@@ -2,12 +2,16 @@ const fs = require('fs')
 const flatted = require('flatted')
 
 const save = (filename,objects ) => {
-  fs.writeFileSync(`./${filename}.json`, flatted.stringify(objects, null, 2))
+    fs.writeFileSync(`./${filename}.json`, flatted.stringify(objects, null, 2))
 }
 
 const load = (filename) => {
     const file = fs.readFileSync(`./${filename}.json` , 'utf8')
     return flatted.parse(file)
 }
+const insert = (filename, object) => {
+    const objects = load(filename)
+    save(filename, objects.concat(objects)) 
+}
 
-module.exports = { save, load }
+module.exports = { save, load, insert }
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const colors = require('colors')
 const db = require('./database')
-//const Guest = require('./guest')
-//const Barber = require('./barber')
+// const Guest = require('./guest')
+// const Barber = require('./barber')
 
-//const hilmi = new Guest('Hilmi')
-//const cem = new Barber('Cem')
+// const hilmi = new Guest('Hilmi')
+// const cem = new Barber('Cem')
 
-//hilmi.book(cem,'Haircut')
-//hilmi.cancel(cem,'16-02-2021 at 14:00')
-//hilmi.change(cem,'16-02-2021 at 14:00')
+// hilmi.book(cem,'Haircut')
+// hilmi.cancel(cem,'16-02-2021 at 14:00')
+// hilmi.change(cem,'16-02-2021 at 14:00')
 
 function printBooking(booking) {
     console.log(`${colors.blue(booking.guest.name)} has ${colors.red(booking.guest.bookings.length)} booking(s) with ${colors.blue(booking.barber.name)} for ${colors.green(booking.guest.demand)} in ${colors.red(booking.guest.date)}`)
@@ -34,10 +34,17 @@ function printChangingHistory(guest) {
 
 const guests = db.load('guests')
 
-guests.forEach(printBookingHistory)
-guests.forEach(printCancellationHistory)
-//guests.forEach(printChangingHistory)
+// guests.forEach(printBookingHistory)
+// guests.forEach(printCancellationHistory)
+// guests.forEach(printChangingHistory)
 
 //printBookingHistory(hilmi)
 //printCancellationHistory(hilmi)
 //printChangingHistory(hilmi)
+
+const semih = new Guest('semih')
+
+db.insert('guests',semih)
+const guests = db.load('guests')
+
+guests.forEach(p => console.log(p.name))
